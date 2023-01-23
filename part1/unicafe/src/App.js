b2e4426a9441289b6540d264e35de8008358adeb
Eutrius <eutrius@gmail.com>
2023-01-23 15:37:37 +0100

    1.7 unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,6 +13,10 @@ const App = () => {
     setState(state + 1);
   };
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all || 0;
+  const positive = (good / all) * 100 || 0;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -26,6 +30,9 @@ const App = () => {
       <p>good {good} </p>
       <p>neutral {neutral} </p>
       <p>bad {bad} </p>
+      <p>all {all} </p>
+      <p>average {average}</p>
+      <p>positive {positive} % </p>
     </div>
   );
 };
