cf50cbdf150fba8a0e083fde1cd137b8289a77e7
Eutrius <eutrius@gmail.com>
2023-01-23 15:49:25 +0100

    1.8 unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,23 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all || 0;
+  const positive = (good / all) * 100 || 0;
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad} </p>
+      <p>all {all} </p>
+      <p>average {average}</p>
+      <p>positive {positive} % </p>
+    </>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -12,10 +29,6 @@ const App = () => {
   const handleClick = (state, setState) => {
     setState(state + 1);
   };
-
-  const all = good + neutral + bad;
-  const average = (good - bad) / all || 0;
-  const positive = (good / all) * 100 || 0;
 
   return (
     <div>
@@ -26,13 +39,7 @@ const App = () => {
         text={"neutral"}
       />
       <Button handleClick={() => handleClick(bad, setBad)} text={"bad"} />
-      <h1>statistics</h1>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad} </p>
-      <p>all {all} </p>
-      <p>average {average}</p>
-      <p>positive {positive} % </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
