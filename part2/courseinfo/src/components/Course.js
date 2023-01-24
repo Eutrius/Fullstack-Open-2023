ba8703d52dedb82c3eb63377bbb4b081b0580709
Eutrius <eutrius@gmail.com>
2023-01-24 17:37:51 +0100

    2.1 Course Information step6

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,43 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map((part) => (
+        <Part part={part} key={part.id} />
+      ))}
+    </>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+    </>
+  );
+};
+
+export default Course;
