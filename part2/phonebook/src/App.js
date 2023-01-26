6a018a1a559cc587d941bd8f439a534ea67ea767
Eutrius <eutrius@gmail.com>
2023-01-26 17:41:58 +0100

    2.8 The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,26 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
 
-  const handleInputChange = (e) => {
-    setNewName(e.target.value);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setnewNumber] = useState("");
+
+  const handleInputChange = (e, setState) => {
+    setState(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!checkForDuplicate(newName)) {
-      setPersons(persons.concat({ name: newName }));
+    if (persons.every(({ name }) => name !== newName)) {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
       setNewName("");
+      setnewNumber("");
+    } else {
+      alert(`${newName} is already added to phonebook`);
     }
-  };
-  const checkForDuplicate = (name) => {
-    for (let { name } of persons) {
-      if (name === newName) {
-        alert(`${newName} is already added to phonebook`);
-        return true;
-      }
-    }
-    return false;
   };
 
   return (
@@ -30,7 +28,19 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleInputChange} value={newName} />
+          name:{" "}
+          <input
+            onChange={(e) => handleInputChange(e, setNewName)}
+            value={newName}
+            required={"required"}
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            onChange={(e) => handleInputChange(e, setnewNumber)}
+            value={newNumber}
+          />
         </div>
         <div>
           <button type="submit" onClick={handleSubmit}>
@@ -39,8 +49,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name }) => (
-        <p key={name}>{name}</p>
+      {persons.map(({ name, number }) => (
+        <p key={name}>
+          {name} {number}
+        </p>
       ))}
     </div>
   );
