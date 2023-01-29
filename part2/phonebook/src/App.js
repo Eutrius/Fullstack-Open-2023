f9d340df17c743957308bee674d06ecbf16e2bab
Eutrius <eutrius@gmail.com>
2023-01-29 12:29:25 +0100

    2.11 The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
+import axios from "axios";
 
 const initialInputs = {
   newName: "",
@@ -10,18 +11,19 @@ const initialInputs = {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [inputs, setInputs] = useState(initialInputs);
   const { newName, newNumber, filter } = inputs;
 
   const filteredPersons = persons.filter(({ name }) =>
     name.toLowerCase().includes(filter)
   );
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
