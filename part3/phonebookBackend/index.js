16e11fe660ab6ec51fbbcba1c1a2d66098a39f39
Eutrius <eutrius@gmail.com>
2023-02-28 21:22:44 +0100

    3.18 Phonebook Database Step

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -25,31 +25,12 @@ app.use(
   })
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "invalid id format" });
+  }
   next(error);
 };
 
@@ -60,22 +41,28 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/info", (request, response) => {
-  response.send(
-    `<div>
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date().toString()}</p>
-    </div>`
+  Person.find({}).then((person) =>
+    response.send(
+      `<div>
+        <p>Phonebook has info for ${person.length} people</p>
+        <p>${new Date().toString()}</p>
+      </div>`
+    )
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -95,11 +82,6 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (checkPersonsNames(body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -134,14 +116,3 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const generateId = () => {
-  const maxId = 1000000;
-  return Math.floor(Math.random() * maxId);
-};
-
-const checkPersonsNames = (name) => {
-  return persons
-    .map((person) => person.name.toLowerCase())
-    .includes(name.toLowerCase());
-};
