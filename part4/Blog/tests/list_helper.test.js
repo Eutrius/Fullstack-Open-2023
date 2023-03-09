db4b25e81169f3754c5123a3396c326dfe9e2f2f
Eutrius <eutrius@gmail.com>
2023-03-09 20:43:20 +0100

    4.4 Helper Function And Unit Tests Step2

--- part4/Blog/tests/list_helper.test.js
+++ part4/Blog/tests/list_helper.test.js
@@ -1,8 +1,30 @@
 const listHelper = require("../utils/list_helper");
 
-test("dummy returns one", () => {
-  const blogs = [];
+describe("dummy", () => {
+  test("dummy returns one", () => {
+    const blogs = [];
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
+});
+
+describe("total likes", () => {
+  const blogsForTest = require("../utils/blogsForTest");
+
+  test("of empty list is zero", () => {
+    expect(listHelper.totalLikes([])).toBe(0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const listWithOneBlog = [blogsForTest[0]];
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(
+      listWithOneBlog[0].likes
+    );
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const expectedLikes = 36;
+    expect(listHelper.totalLikes(blogsForTest)).toBe(expectedLikes);
+  });
 });
