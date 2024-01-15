19bf7917982de88b7f4ec2646cbf26061922ed03
Eutrius <eutrius@gmail.com>
2024-01-15 19:29:14 +0100

    4.10 Blog List Tests step3

--- part4/Blog/tests/blogs.test.js
+++ part4/Blog/tests/blogs.test.js
@@ -1,26 +1,61 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const Blog = require("../models/blog.js");
+const blogsForTest = require("../utils/blogsForTest.js");
 
 const api = supertest(app);
 
+beforeAll(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(blogsForTest);
+});
+
 describe("HTTP GET", () => {
-  test("blogs in correct amount returned in JSON", async () => {
+  test("blogs are returned in JSON", async () => {
     const response = await api.get("/api/blogs");
 
     expect(response.status).toBe(200);
     expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.body).toHaveLength(2);
   });
-});
 
-describe("Unique ID", () => {
-  test("unique identity property of a blog is named id", async () => {
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(blogsForTest.length);
+  });
+
+  test("blogs have a unique property named id", async () => {
     const response = await api.get("/api/blogs");
 
     response.body.forEach((blog) => {
       expect(blog.id).toBeDefined();
     });
+  });
+});
+
+describe("HTTP POST", () => {
+  test("a blog can be created", async () => {
+    const newBlog = {
+      title: "Create A Fullstack Website",
+      author: "Fullstack Open",
+      url: "https://fullstackopen.com",
+      likes: 99,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAfter = await api.get("/api/blogs");
+
+    expect(blogsAfter.body).toHaveLength(blogsForTest.length + 1);
+
+    const titlesAfter = blogsAfter.body.map((blog) => blog.title);
+
+    expect(titlesAfter).toContain("Create A Fullstack Website");
   });
 });
 
