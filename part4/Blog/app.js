6bc58d59c0e7fbcf90f386f70e9c03c1a6dda1c8
Eutrius <eutrius@gmail.com>
2024-01-17 21:42:44 +0100

    4.16 Bloglist Expansion step4

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { info } = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const { MONGODB_URI } = require("./utils/config");
 const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
@@ -23,9 +24,14 @@ mongoose
   });
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/Blog/controllers/users.js
+++ part4/Blog/controllers/users.js
@@ -8,21 +8,29 @@ usersRouter.get("/", async (request, response) => {
 });
 
 usersRouter.post("/", async (request, response) => {
-  console.log(request.body);
   const { username, name, password } = request.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash("password", saltRounds);
+  const minLength = 3;
+  if (username.length < minLength || password.length < minLength) {
+    response
+      .status(400)
+      .send({
+        error: "Both Username and Password must be at least 3 Characters long",
+      });
+  } else {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash("password", saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
 
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
-  response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
+  }
 });
 
 module.exports = usersRouter;
