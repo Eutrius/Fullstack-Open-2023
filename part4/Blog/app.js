75917d4e78bb4de9d0f9b3eca0b3351e897c1c0b
Eutrius <eutrius@gmail.com>
2024-01-17 16:13:42 +0100

    4.15 Bloglist Expansion step3

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { info } = require("./utils/logger");
 const { MONGODB_URI } = require("./utils/config");
+const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
 
 mongoose.set("strictQuery", false);
@@ -24,6 +25,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
