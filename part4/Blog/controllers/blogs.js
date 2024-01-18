da1909cca3fb88555665b2bdf53733a8b0d29411
Eutrius <eutrius@gmail.com>
2024-01-18 17:34:48 +0100

    4.19 Bloglist Expansion step7

--- part4/Blog/controllers/blogs.js
+++ part4/Blog/controllers/blogs.js
@@ -1,15 +1,28 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
+
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const users = await User.find({});
-  const user = users[0];
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    response.status(401).json({ error: "Invalid token" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     ...body,
