22dddfe7c5f7f92cdbb2891993354bfaf6766778
Eutrius <eutrius@gmail.com>
2024-01-18 19:01:00 +0100

    4.21 Bloglist Expansion step9

--- part4/Blog/controllers/blogs.js
+++ part4/Blog/controllers/blogs.js
@@ -33,7 +33,24 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
+  const blog = await Blog.findById(request.params.id);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    response.status(401).json({ error: "Invalid token" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  if (blog.user.toString() !== user.id) {
+    response.status(401).json({ error: "Unauthorized token" });
+  }
+  await Blog.findByIdAndDelete(blog._id);
+
+  const idFromUserInString = blog._id.toString();
+  user.blogs = user.blogs.filter(
+    (blogId) => blogId.toString() !== idFromUserInString,
+  );
+
+  await user.save();
   response.status(204).end();
 });
 
